8c8a8a1b5bb746b22321647747c549650db145dd
rainwang007 <rainwangjiantao@gmail.com>
2023-08-24 16:27:33 +0800

    backend frame built

---
+++ config.py
@@ -0,0 +1,13 @@
+    # config.py
+MYSQL_CONFIG = {
+    'host': 'localhost',
+    'user': 'username',
+    'password': 'password',
+    'database': 'dating_app'
+}
+
+MONGODB_CONFIG = {
+    'host': 'localhost',
+    'port': 27017,
+    'database': 'dating_app'
+}
