8739b8ed5834777f637d0a5a1b57aa2d6708b1cf
rainwang007 <rainwangjiantao@gmail.com>
2023-09-01 11:05:42 +0800

    add models

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('../config.py')
+    app.config['SQLALCHEMY_DATABASE_URI'] = '你的数据库连接字符串'
 
     jwt = JWTManager(app)
 
@@ -11,7 +16,8 @@ def create_app():
     from .profile.routes import profile_bp
     from .match.routes import match_bp
     from .chat.routes import chat_bp
-
+    
+    db.init_app(app)
     app.register_blueprint(auth_bp)
     app.register_blueprint(profile_bp)
     app.register_blueprint(match_bp)
---
+++ models/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(64))
+    age = db.Column(db.Integer)
+    location = db.Column(db.String(128))
+    bio = db.Column(db.Text)
+    avatar_url = db.Column(db.String(256))
+
+class Match(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user1_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user2_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    status = db.Column(db.String(16))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Chat(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    match_id = db.Column(db.Integer, db.ForeignKey('match.id'), nullable=False)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    message = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+# 创建表
+db.create_all()
